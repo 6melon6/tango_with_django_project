0cc7dae7ee114bc47c2339f2c5a9f4bfd41105ea
Rui Dong <3138425d@student.gla.ac.uk>
2026-03-10 18:52:15 +0000

    fix allowed hosts

--- diet_tracker/settings.py
+++ diet_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kfd)+6hv4#sk25cc-^b074+&2h_&rw@8h_$z%hda5b+9mql-5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
